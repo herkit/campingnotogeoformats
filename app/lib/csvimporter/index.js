2de82a6bd80d14a021e3656041d79e23aa4a70c0
Henrik Grotle <henrik.grotle@linkmobility.com>
2016-07-13 14:58:08 +0200

    removed comment

--- app/lib/csvimporter/index.js
+++ app/lib/csvimporter/index.js
@@ -2,27 +2,6 @@ var csv = require('csv');
 var fs = require('fs');
 var GeoJSON = require('geojson');
 
-/*
--114.353,
-53.675,
-ALBE/Alberta Beach Family RV Park   CP PH:780.924.2333 mid may-late sep  SITES:30  AMEN:WES approx  mi  of Alberta Beach  53.675 -114.353,
-ALBE,
-Alberta Beach Family RV Park ,
-CP,
-780.924.2333,
-mid may-late sep,
- ,
-30,
- ,
-WES ,
-AB ,
-,
-,
-Alberta Beach 
-
-
-*/
-
 var csvimport = function(stream, options, callback) {
   var parse = csv.parse();
   var csvoptions = options.csv || {};
